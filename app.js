3a05722760c26e559403891ca5199b00b73dd12e
AureLucile <aurelie.sales1@gmail.com>
2017-03-15 19:18:26 +0100

    Essec is better than world.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static("static"));
 
 app.get("/hello",function(req,res) {
-	res.send("Hello World");
+	res.send("Hello Essec");
 });
 
 app.listen(2432,function(err) {
